665b3a66dc85528221023210c3496518f8eec860
Kribas <carlousrimal@gmail.com>
2025-05-20 10:55:48 +0545

    initial commit

--- server.js
+++ server.js
@@ -1,20 +1,28 @@
 import http from "http";
+import url from 'url'
+import path from 'path'
+import fs from 'fs'
 
 const PORT = process.env.PORT;
+const __filename = url.fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
-const server = http.createServer((req, res) => {
+const server = http.createServer(async (req, res) => {
   try {
     if (req.method === "GET") {
+      let filePath
       if (req.url === "/") {
-        res.writeHead(200, { "Content-Type": "text/html" });
-        res.end("<h1>HomePage</h1>");
+        filePath = path.join(__dirname, 'public', 'index.html')        
       } else if (req.url === "/about") {
-        res.writeHead(200, { "Content-Type": "text/html" });
-        res.end("<h1>AboutPage</h1>");
+        filePath = path.join(__dirname, 'public', 'about.html')
       } else {
-        res.writeHead(200, { "Content-Type": "text/html" });
-        res.end("<h1>Not Found</h1>");
+        throw new Error('Not Found')
       }
+      const data = await fs.readFile(filePath)
+      res.setHeader('Content-Type', 'text/html')
+      res.write(data)
+      res.end()
+
     } else {
       throw new Error("Method not allowed");
     }
