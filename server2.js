46ab9d744296ab0cd122523228d6d71918f36b5f
Kribas <carlousrimal@gmail.com>
2025-05-20 20:47:56 +0545

    created middlewares for users data and bug fixed

--- server2.js
+++ server2.js
@@ -17,27 +17,57 @@ let users = [
   },
 ];
 
-const server = createServer((req, res) => {
-    if(req.url === '/api/users' && req.method === 'GET') {
-        res.setHeader('Content-Type', 'application/json')
-        res.write(JSON.stringify(users))
-        res.end()
-    } else if(req.url.match(/\/api\/users\/([0-9]+)/) && req.method === 'GET') {
-        const id = req.url.split('/')[3]
+// Logger Middleware
+const logger = (req,res,next) => {
+  console.log(`${req.method} ${req.url}`);
+  next()
+}
 
-        res.setHeader('Content-Type', 'application/json')
-        res.write(JSON.stringify({id: 1, name: 'John Doe'}))
-        res.end()
-    } 
-    
-    else {
-        res.setHeader('Content-Type', 'application/json')
-        res.statusCode = 404
-        res.write(JSON.stringify({message: 'Route not found'}))
-        res.end()
-    }
-})
+// JSON Middleware
+const jsonMiddleware = (req,res,next) => {
+  res.setHeader("Content-Type", "application/json")
+  next()
+}
+
+//Route handler for GET /api/users
+const getUsersHandler = (req,res) => {
+  res.write(JSON.stringify(users))
+  res.end()
+}
+
+const getUsersByIdHandler = (req,res) => {
+  const id = req.url.split('/')[3]
+  const user = users.find((user) => user.id === parseInt(id))
+  if(user) {
+    res.write(JSON.stringify(user))
+  } else {
+    res.statusCode = 404
+    res.write(JSON.stringify({message: "User not found"}))
+  }
+  res.end()
+}
+
+const notFoundHandler = (req, res) => {
+  res.statusCode = 404
+  res.write(JSON.stringify({message: "Route not found"}))
+  res.end()
+}
+
+
+const server = createServer((req, res) => {
+  logger(req,res, () => {
+    jsonMiddleware(req, res, () => {
+      if(req.url === "/api/users" && req.method === "GET") {
+        getUsersHandler(req, res)
+      } else if(req.url.match(/\/api\/users\/([0-9]+)/) && req.method === "GET") {
+        getUsersByIdHandler(req,res)
+      } else {
+        notFoundHandler(req, res)
+      }
+    })
+  })
+});
 
 server.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-  });
+  console.log(`Server running on port ${PORT}`);
+});
